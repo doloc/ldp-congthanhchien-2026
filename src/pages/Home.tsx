180639f04e6725fb5db9c154a4fac91093fbf3c0
LocDD <locdd3@tnexfinance.com.vn>
2026-05-23 01:33:51 +0700

    fix build

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,14 +2,11 @@ import Section1Hero from '../components/Section1Hero'
 import Section2Video from '../components/Section2Video'
 import Section3Leaderboard from '../components/Section3Leaderboard'
 
-// TODO: replace with actual YouTube embed URL, e.g. "https://www.youtube.com/embed/XXXXXXXXX"
-const VIDEO_EMBED_URL = ''
-
 export default function Home() {
   return (
     <main className="w-full overflow-x-hidden">
       <Section1Hero />
-      <Section2Video videoEmbedUrl={VIDEO_EMBED_URL} />
+      <Section2Video />
       <Section3Leaderboard />
     </main>
   )
